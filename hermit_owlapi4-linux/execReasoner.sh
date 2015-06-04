9a1387722452d79f2a0bfb21f25c54e5657ca767
ignazio <ignazio.palmisano@gmail.com>
2015-06-04 22:32:34 +0100

    fix issue with HermiT class name and functional syntax comments

--- hermit_owlapi4-linux/execReasoner.sh
+++ hermit_owlapi4-linux/execReasoner.sh
@@ -5,5 +5,5 @@
 # Last updated: 27-Mar-13
 # 
 timeoutsec=`expr $1 / 1000`
-timeout $timeoutsec java -Xmx$2 -jar OREv2ReasonerWrapper.jar org.semanticweb.HermiT.Reasoner.ReasonerFactory $3 $4 $5 $6 $7 $8 $9
+timeout $timeoutsec java -Xmx$2 -jar OREv2ReasonerWrapper.jar org.semanticweb.HermiT.Reasoner$ReasonerFactory $3 $4 $5 $6 $7 $8 $9
 		
